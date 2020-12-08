46cd40c1037eed3dde41e581bc3149558b1b85f1
PriscaTonia <priscaebube@gmail.com>
2020-12-08 10:54:08 +0100

    Navbar Designs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import Navbar from './components/Navbar'
+import  {BrowserRouter, Route } from 'react-router-dom'
+import LandingPage from './components/LandingPage'
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-     <h1> Hello!</h1>
+    
+     <Navbar />
+     <Route path='/' Component={LandingPage} />
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+    const [className, setClassName] = useState('NavBar');
+    
+    const handleClick = () =>{
+       
+        setClassName('NavBar-mobile')
+        if(className === 'NavBar-mobile'){
+            setClassName('NavBar')
+        }
+    }
+    
+    
+    return (
+        <nav>
+            <div className='container nav'>
+                <a href='#' className='brand'>Eva <span>Kitchen</span></a>
+
+                <button className='hamburger' onClick={handleClick}>
+                        <i className='fa fa-bars'></i>
+                </button>
+                <ul className= {className}>
+                    <li ><Link className='navLink active' to='/'> Home </Link> </li>
+                    <li ><Link className='navLink' to='/menu'> Menu </Link> </li>
+                    <li ><Link className='navLink' to='/about'> About Us </Link> </li>
+                    <li ><Link className='navLink' to='/contact'> Contact Us </Link> </li>
+                    
+                </ul>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
