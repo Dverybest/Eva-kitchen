4aa4187c2330c9318c9d4627f1f86571525268ca
PriscaTonia <priscaebube@gmail.com>
2020-12-14 10:56:24 +0100

    This is the Hero-section for the landing page along with its mobile view

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -11,13 +11,29 @@ const LandingPage = () => {
                     <div className='hero-info'>
                         <h1>Your Favourite Food Delivered Hot And Fresh</h1>
                         <p>Satisfy your craving by getting the food you want delivered to you fast. EvaKitchen is now available in selected areas in Enugu.</p>
-                        <button>Order Now     <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
+                        <button>Order Now                  <i class="fa fa-arrow-right" aria-hidden="true"></i></button>
                     </div>
                     <div className='hero-image'>
                         <img src={Dish1} alt='' />
                     </div>
                 </div>
-                <div className='hero-contact'></div>
+                <div className='hero-contact'>
+                    <div className='hero-contact-info clock'>
+                        <div className='icons'><i class="fa fa-clock-o" aria-hidden="true"></i></div>
+                        <span>10:00am - 07:00pm</span>
+                        <span>Working Hours</span>
+                    </div>
+                    <div className='hero-contact-info'>
+                        <div className='icons'><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+                        <span>Independence Layout Enugu</span>
+                        <span>Get Direction</span>
+                    </div>
+                    <div className='hero-contact-info phone'>
+                        <div className='icons'><i class="fa fa-phone" aria-hidden="true"></i></div>
+                        <span>+234 706 121 1353</span>
+                        <span>Call Online</span>
+                    </div>
+                </div>
             </section>
             
         </div>
