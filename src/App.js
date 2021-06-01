1dc86fb38ef8303333eec682c97bd07dd68b79b3
Nancy Rike <nancyrike7@gmail.com>
2021-06-01 15:29:43 +0100

    added provider

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import LandingPage from './pages/landingPage/LandingPage'
 import MenuPage from './pages/menuPage/MenuPage'
 import AboutPage from './pages/aboutPage/AboutPage'
 import ContactPage from './pages/contactPage/ContactPage';
-
+import { NotificationContainer } from "react-notifications";
+import { Provider } from 'react-redux'
 
 function App() {
   return (
-    <BrowserRouter>   
-      <Switch>
-        <Route exact path='/' component={LandingPage} />
-        <Route exact path='/menu' component={MenuPage} />
-        <Route exact path='/about' component={AboutPage} />
-        <Route exact path='/contact-us' component={ContactPage} />
-      </Switch>  
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>   
+        <Switch>
+          <Route exact path='/' component={LandingPage} />
+          <Route exact path='/menu' component={MenuPage} />
+          <Route exact path='/about' component={AboutPage} />
+          <Route exact path='/contact-us' component={ContactPage} />
+        </Switch>  
+        <NotificationContainer/>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
