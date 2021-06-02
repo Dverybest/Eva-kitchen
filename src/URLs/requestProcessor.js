6edeab0661d232ea633fccc46579cd7c0752319c
Nancy Rike <nancyrike7@gmail.com>
2021-06-02 22:40:09 +0100

    request processor corrections

--- src/URLs/requestProcessor.js
+++ src/URLs/requestProcessor.js
@@ -1,22 +1,24 @@
-import { axios } from " axios ";
+import axios from "axios";
 import { BASEURL } from "./config";
-import {  } from "module";
 
 axios.defaults.baseURL = BASEURL;
-const requestProcessor = async({
-    method, url, payload, dispatch,
-});
+export const requestProcessor = async({
+    method,
+    url, 
+    payload, 
+})=>{;
 try {
     const result = await axios ({
         method,
         url,
         data:payload,
-    }),
-    return{status: result.status, ...result.data};
+    })
+    return {status: result.status, ...result.data};
 }
 catch(error){
     if (error?.response) {
         return{status: error.response.status, ...error.response.data};
     }
     return error;
+}
 }
