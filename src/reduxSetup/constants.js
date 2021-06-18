16b94af4718d3ae357a31b68ebcfb1f5622ea962
Nancy Rike <nancyrike7@gmail.com>
2021-06-18 16:27:11 +0100

    redux setup for orderPage

--- src/reduxSetup/constants.js
+++ src/reduxSetup/constants.js
@@ -3,3 +3,6 @@ export const POPULAR_MENU = 'POPULAR_MENU';
 export const SPECIAL_MENU = "SPECIAL_MENU";
 export const MENU_CATEGORY = "MENU_CATEGORY";
 export const ALL_MENU = 'ALL_MENU';
+export const ORDER_MENU = 'ORDER_MENU';
+export const ITEMS_IN_CART = 'ITEMS_IN_CART';
+export const ADD_TO_CART = 'ADD_TO_CART';
