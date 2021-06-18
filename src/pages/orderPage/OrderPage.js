b04eaa6b606aea8e78947ee2b8d75d8f719c0fee
Nancy Rike <nancyrike7@gmail.com>
2021-06-18 16:11:40 +0100

    UI and API for order page

--- src/pages/orderPage/OrderPage.js
+++ src/pages/orderPage/OrderPage.js
@@ -1,38 +1,69 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Footer  from "../../components/footer/Footer";
-import Navbar from "../../components/navbar/Navbar";
-import orangeStar  from "../../assets/orangeStar.svg";
-import jollofRice  from "./orderImages/jollofRice.png";
+import HeaderNavbar from "./orderNavbar/OrderNavbar";
 import './OrderPage.css';
-const OrderPage = ()=> {
+import { connect } from 'react-redux';
+import {getMenuAction} from "../../reduxSetup/actions/allMenuAction";
+import {addToCartAction,} from "../../reduxSetup/actions/orderMenuAction";
+import Dish from '../../components/reusableMenuCard/Dish';
+
+
+const OrderPage = (props)=> {
+    const meal = props.location.state;        
+    const [quantity, setquantity] = useState(
+        0
+    )
+    const addItemToCart =()=>{
+        props.addToCartAction({...meal, quantity});
+    }
+    const handleIncrement=()=>{
+        setquantity(quantity + 1);
+    }
+    const handleDecrement=()=>{
+        if (quantity === 0){
+            return
+        }
+        setquantity(quantity - 1);
+    }
+    
+    useEffect(() => {
+        props.getMenuAction();        
+    }, []);
     return (
         <div>
-            <Navbar/>
-            <section className="p-5 mt-3">
-                <div className="container p-5">
-                    <div className="row">
-                        <div className="col-md-5">
-                            <img src={jollofRice} alt=""/>
+            <div>
+                <HeaderNavbar/>
+               <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span> 
+            </div>
+            <section className="orderSection p-5 mt-3">
+                <div className="container p-5 ">
+                    <div className="row d-flex align-items-center justify-content-between">
+                        <div className="col-sm-6">
+                            <div className='mealImage'>
+                                <img src={meal.image} alt="" className="mealImg"/>
+                            </div>
                         </div>
-                        <div className="col-md-7">
-                            <h4 className="price">N1000</h4>
-                            <div className="py-4">
-                                <span className="numberButton">-</span>
-                                <span className="mx-2">1</span>
-                                <span className="numberButton">+</span>
-                            </div>
-                            <h6 className="mealName">Fried Rice</h6>
-                            <p className='description'>Fried rice with chicken laps</p>
-                            <h6 className="priceTotal">N 1400</h6>
-                            <div className="my-3">
-                                <span><img src={orangeStar} alt=""/></span>
-                                <span><img src={orangeStar} alt=""/></span>
-                                <span><img src={orangeStar} alt=""/></span>
-                                <span><img src={orangeStar} alt=""/></span>
-                                <span><img src={orangeStar} alt=""/></span>
-                            </div>
-                            <div>
-                                <button className="btn cartButton">Add to Cart</button>
+                        <div className="col-sm-6">
+                            <div className="mealDetails">
+                                <h4 className="price">{meal.price}</h4>
+                                <div className="py-4">
+                                    <span className="numberButton" onClick={handleDecrement} disabled={quantity < 1}><i class="fa fa-minus" aria-hidden="true"></i></span>
+                                    <span className="mx-2">{quantity}</span>
+                                    <span className="numberButton" onClick={handleIncrement}><i class="fa fa-plus" aria-hidden="true"></i></span>
+                                </div>
+                                <h6 className="mealName">{meal.name}</h6>
+                                <p className='description'>{meal.description}</p>
+                                <h6 className="priceTotal">{meal.price}</h6>
+                                <div className="my-3">
+                                    <span className="starColourOrange"><i class="fa fa-star" aria-hidden="true"></i></span>
+                                    <span className="starColourOrange"><i class="fa fa-star" aria-hidden="true"></i></span>
+                                    <span className="starColourOrange"><i class="fa fa-star" aria-hidden="true"></i></span>
+                                    <span className="starColourOrange"><i class="fa fa-star-half-o" aria-hidden="true"></i></span>
+                                    <span className="starColourOrange"><i class="fa fa-star-o" aria-hidden="true"></i></span>
+                                </div>
+                                <div>
+                                    <button className="btn cartButton" onClick={addItemToCart}>Add to Cart</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -46,36 +77,9 @@ const OrderPage = ()=> {
                 <div className="container">
                     <h3 className="tabHeading">Related Dishes</h3>
                     <div className="row">
-                        <div className="col-md-4">
-                            <div className="p-1">
-                                <img src={jollofRice} alt=""/>
-                                <div className="d-flex align-items-center justify-content-between ">
-                                    <p className= 'dishName'>Jolof Rice</p>
-                                    <h6 className="dishPrice">800</h6>
-                                </div>
-                                <div><p className='dishDescription'>Nigerian party jollof rice ..</p></div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="p-1">
-                                <img src={jollofRice} alt=""/>
-                                <div className="d-flex align-items-center justify-content-between ">
-                                    <p className= 'dishName'>Jolof Rice</p>
-                                    <h6 className="dishPrice">800</h6>
-                                </div>
-                                <div><p className='dishDescription'>Nigerian party jollof rice ..</p></div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="p-1">
-                                <img src={jollofRice} alt=""/>
-                                <div className="d-flex align-items-center justify-content-between ">
-                                    <p className= 'dishName'>Jolof Rice</p>
-                                    <h6 className="dishPrice">800</h6>
-                                </div>
-                                <div><p className='dishDescription'>Nigerian party jollof rice ..</p></div>
-                            </div>
-                        </div>
+                        {props.allMenu?.slice(0,3).map((dish, index)=>{
+                                return <Dish key={index} {...dish} />
+                        })}
                     </div>
                 </div>
             </section>
@@ -83,36 +87,9 @@ const OrderPage = ()=> {
                 <div className="container">
                     <h3 className="tabHeading">other Dishes</h3>
                     <div className="row">
-                        <div className="col-md-4">
-                            <div className="p-1">
-                                <img src={jollofRice} alt=""/>
-                                <div className="d-flex align-items-center justify-content-between ">
-                                    <p className= 'dishName'>Jolof Rice</p>
-                                    <h6 className="dishPrice">800</h6>
-                                </div>
-                                <div><p className='dishDescription'>Nigerian party jollof rice ..</p></div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="p-1">
-                                <img src={jollofRice} alt=""/>
-                                <div className="d-flex align-items-center justify-content-between ">
-                                    <p className= 'dishName'>Jolof Rice</p>
-                                    <h6 className="dishPrice">800</h6>
-                                </div>
-                                <div><p className='dishDescription'>Nigerian party jollof rice ..</p></div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="p-1">
-                                <img src={jollofRice} alt=""/>
-                                <div className="d-flex align-items-center justify-content-between ">
-                                    <p className= 'dishName'>Jolof Rice</p>
-                                    <h6 className="dishPrice">800</h6>
-                                </div>
-                                <div><p className='dishDescription'>Nigerian party jollof rice ..</p></div>
-                            </div>
-                        </div>
+                        {props.allMenu?.slice(0,3).map((dish, index)=>{
+                            return <Dish key={index} {...dish} />
+                        })}
                     </div>
                 </div>
             </section>
@@ -121,4 +98,12 @@ const OrderPage = ()=> {
         </div>
     )
 }
-export default OrderPage;
+const mapStateToProps=(state)=>{
+    return{
+        allMenu: state.meals.allMenu,
+    };
+};
+const connector = connect(mapStateToProps,  {
+    getMenuAction, addToCartAction,
+});
+export default connector(OrderPage);
