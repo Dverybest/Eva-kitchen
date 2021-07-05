a4530aebe19d4c02bb2e0e94e44f8a7aeef1bf82
Charles Best <charlesbest8@gmail.com>
2021-07-05 10:17:06 +0100

    refactor

--- src/reduxSetup/actions/allMenuAction.js
+++ src/reduxSetup/actions/allMenuAction.js
@@ -2,26 +2,25 @@ import { requestProcessor } from "../../URLs/requestProcessor";
 import { ALL_MENU } from "../constants";
 import { loadingAction } from "./loadingAction";
 
-const getMenuAction = ( id) => (dispatch) =>
-new Promise(async(resolve, reject) => {
-    dispatch (loadingAction(true));
-    try {
-        const response = await requestProcessor ({
-            method: 'GET',
-            url: id? `/menu?category=${id}`:'/menu' ,
-            dispatch,  
-        });
-        if (response.success) {
-            dispatch(loadingAction(false));
+const getMenuAction = (id) => (dispatch) =>
+    new Promise(async (resolve, reject) => {
+        dispatch(loadingAction(true));
+        try {
+            const response = await requestProcessor({
+                method: 'GET',
+                url: id ? `/menu?category=${id}` : '/menuuc',
+                dispatch,
+            });
             dispatch({
                 type: ALL_MENU,
                 payload: response.data.docs
             })
             resolve(response);
+        } catch (error) {
+            reject(error)
+
+        }finally{
+            dispatch(loadingAction(false));
         }
-    } catch (error) {
-        reject(error)
-        
-    }
-})
-export {getMenuAction};
+    })
+export { getMenuAction };
