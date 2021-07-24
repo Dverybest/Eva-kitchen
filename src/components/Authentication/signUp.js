d079dc04c433c4d1ffb15ba63213938ddce88968
Nancy Rike <nancyrike7@gmail.com>
2021-07-24 09:09:43 +0100

    Api integration for sign-up page

--- src/components/Authentication/signUp.js
+++ src/components/Authentication/signUp.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React,{useState} from "react";
 import './authentication.css';
 import {useHistory} from "react-router-dom";
+import { NotificationManager } from "react-notifications";
+import { connect } from "react-redux";
+import { signUpAction } from "../../reduxSetup/actions/authAction";
 
-const SignUp = ({setmanageAuth}) => {
+const SignUp = (props) => {
     const history =  useHistory();
+    const [signUpDetails, setsignUpDetails] = useState({
+      username: "",
+      password: "",
+      phoneNumber: "",
+      email: '',
+    })
+      const handleChange =(e)=>{
+      e.preventDefault()
+      setsignUpDetails({
+        ...signUpDetails,
+        [e.target.name]: e.target.value
+      })
+    }
+    const handleSignUp =(e)=>{
+      e.preventDefault();
+      if(!/\S+@\S+\.\S+/.test(signUpDetails.email)) return NotificationManager.error('Invalid email address');
+      if(!signUpDetails.password) return NotificationManager.error('Password cannot be empty')
+      props.signUpAction(signUpDetails)
+      .then(res=>{
+        console.log(res);
+        NotificationManager.success(res.message)
+      }).catch(err=>NotificationManager.error(err.message))
+    }
+
     return (
     <>
       <div className="px-5 py-4 formHolder">
@@ -15,6 +42,9 @@ const SignUp = ({setmanageAuth}) => {
                 type="text"
                 className="form-control inputDetails"
                 placeholder="Jane Doe"
+                required
+                value={signUpDetails.username}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group ">
@@ -23,6 +53,9 @@ const SignUp = ({setmanageAuth}) => {
                 type="text"
                 className="form-control inputDetails"
                 placeholder="example@email.com"
+                required
+                value={signUpDetails.email}
+                onChange={handleChange}
               />
             </div>
             <div className="form-group ">
@@ -31,6 +64,9 @@ const SignUp = ({setmanageAuth}) => {
                 type="text"
                 className="form-control inputDetails"
                 placeholder="+234 xxxxxxxxx"
+                value={signUpDetails.phoneNumber}
+                required
+                onChange={handleChange}
               />
             </div>
             <div className="form-group">
@@ -39,12 +75,18 @@ const SignUp = ({setmanageAuth}) => {
                 type="password"
                 className="form-control inputDetails"
                 placeholder="*******"
+                value={signUpDetails.password}
+                required
+                onChange={handleChange}
               />
             </div>
           </div>
           <div className="d-flex justify-content-end align-items-rigt">
-            <button  className="btn registerButton activeButton">Create Account</button>
-            <button  onClick={()=>setmanageAuth("signIn")} className="btn registerButton ">Sign In</button>
+            <button  onClick={handleSignUp} className="btn registerButton activeButton">Create Account</button>
+            <button  onClick={(e)=>{
+                e.preventDefault();
+                props.setmanageAuth("signIn")
+              }} className="btn registerButton ">Sign In</button>
           </div>
         </form>
         
@@ -52,5 +94,5 @@ const SignUp = ({setmanageAuth}) => {
     </>
   );
 };
-export default SignUp;
+export default connect(null,{signUpAction})(SignUp);
 
